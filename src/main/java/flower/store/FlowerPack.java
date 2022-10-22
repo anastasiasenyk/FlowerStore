2fdd1073cb6f4b1d4f6e4d87b4626984390899e5
mousey <91616572+anastasiasenyk@users.noreply.github.com>
2022-10-22 10:58:41 +0300

    java doc

--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -1,5 +1,11 @@
 /**
- * package flower.store
+ * Domain classes used to produce flower store
+ *
+ * These classes contain the flower properties
+ *
+ * @since 1.0
+ * @author Anastasiia Senyk
+ * @version 1.0
  */
 package flower.store;
 
