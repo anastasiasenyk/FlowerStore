59c2ddb9097c02c6e1cb9d55dc5e109d0a9adb62
mousey <91616572+anastasiasenyk@users.noreply.github.com>
2022-10-23 10:08:30 +0300

    new java-info file

--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -1,12 +1,3 @@
-/**
- * Domain classes used to produce flower store
- *
- * These classes contain the flower properties
- *
- * @since 1.0
- * @author Anastasiia Senyk
- * @version 1.0
- */
 package flower.store;
 
 import lombok.Getter;
---
+++ src/main/java/flower/store/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Domain classes used to produce flower store
+ * These classes contain the flower properties
+ *
+ * @since 1.0
+ * @author Anastasiia Senyk
+ * @version 1.0
+ */
+package flower.store;
